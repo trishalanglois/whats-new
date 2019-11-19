75bdd0b97361094d3edd7fd4e83cb34fd2f81211
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-11-19 14:49:04 -0700

    Add buttons and hover styling

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,7 +3,12 @@ import './Menu.css'
 
 const Menu = () => (
   <div className = 'menu'>
-    This is the menu.
+    <h1>What's <span id='new-feature-word'>New?</span></h1>
+    <button>Local News</button>
+    <button>Technology</button>
+    <button>Entertainment</button>
+    <button>Science</button>
+    <button>Health</button>
   </div>
 );
 
