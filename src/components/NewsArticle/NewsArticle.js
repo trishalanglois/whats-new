0460415bb072aa7f8ede1d63e1a0adb34ca01842
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-11-19 14:17:32 -0700

    Add style for articles and apply grid to app component

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -3,12 +3,12 @@ import './NewsArticle.css';
 
 const NewsArticle = ({image, headline, description, url}) => {
   return (
-    <div className = 'news-article'>
-      <img src = {image} />
+    <article className = 'news-article'>
+      <img src = {image} alt={description}/>
       <h1>{headline}</h1>
       <p>{description}</p>
       <a href={url}>Link to Article</a>
-    </div>
+    </article>
   )
 }
 
