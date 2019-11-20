d22c10fd8fb43ee9b8145cda6c72ab652708a644
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-11-19 18:07:36 -0700

    Filter most recently displayed articles using a keyword

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 import './SearchForm.css';
 
 class SearchForm extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       keyword: ''
     }
+  }
+
+
+  handleChange = event => {
+    this.setState({ keyword: event.target.value })
   }
 
   render () {
     return (
       <header>
         <form className = 'search-form'>
-          <input type='search' placeholder='Search for a news article here' size='70' aria-label='find article based on keyword' />
+          <input type='search' placeholder='Search for a news article here' size='70' value={this.state.keyword} onChange={event => this.handleChange(event)} aria-label='find article based on keyword' />
         </form>
-        <button id='search-button'>Search Now</button>
+        <button id='search-button' onClick= {event =>
+          this.props.filterArticles(event, this.state.keyword)
+        }>Search Now</button>
       </header>
     )
   }
