4e48349c15412002368632cd0db813352f679777
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2019-11-21 15:10:16 -0700

    Add tests for Menu buttons to make sure onClick function is being hit

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -11,11 +11,16 @@ const Menu = (props) => {
   return (
     <nav className = 'menu'>
     <h1 id='logo'>News Your Own <span id='new-feature-word'>Adventure</span></h1>
-    <button onClick = {() => props.changeNews(props.local)} className='menu-button'><img className='menu-button-icon' src={youAreHere} />  Local News</button>
-    <button onClick = {() => props.changeNews(props.technology)}className='menu-button'><img className='menu-button-icon' src={technologyIcon} /> Technology</button>
-    <button onClick = {() => props.changeNews(props.entertainment)}className='menu-button'><img className='menu-button-icon' src={entertainmentIcon} /> Entertainment</button>
-    <button onClick = {() => props.changeNews(props.science)}className='menu-button'><img className='menu-button-icon' src={scienceIcon} /> Science</button>
-    <button onClick = {() => props.changeNews(props.health)}className='menu-button'><img className='menu-button-icon' src={healthIcon} /> Health</button>
+
+    <button onClick = {() => props.changeNews(props.local)} className='menu-button' id='local-button'><img className='menu-button-icon' src={youAreHere} />  Local News</button>
+
+    <button onClick = {() => props.changeNews(props.technology)}className='menu-button' id='technology-button'><img className='menu-button-icon' src={technologyIcon} /> Technology</button>
+
+    <button onClick = {() => props.changeNews(props.entertainment)}className='menu-button' id='entertainment-button'><img className='menu-button-icon' src={entertainmentIcon} /> Entertainment</button>
+
+    <button onClick = {() => props.changeNews(props.science)}className='menu-button' id='science-button'><img className='menu-button-icon' src={scienceIcon} /> Science</button>
+
+    <button onClick = {() => props.changeNews(props.health)}className='menu-button' id='health-button'><img className='menu-button-icon' src={healthIcon} /> Health</button>
   </nav>
 )};
 
--- src/components/Menu/Menu.test.js
+++ src/components/Menu/Menu.test.js
@@ -10,4 +10,49 @@ describe('Menu', () => {
       />)
       expect(wrapper).toMatchSnapshot();
   })
+
+  it('should call the changeNews prop with the local argument when the Local button is clicked', () => {
+    const mockClick = jest.fn();
+    const wrapper = shallow(<Menu
+        changeNews={mockClick}
+      /> );
+    wrapper.find('#local-button').simulate('click');
+    expect(mockClick).toHaveBeenCalled();
+  })
+
+  it('should call the changeNews prop with the technology argument when the Technology button is clicked', () => {
+    const mockClick = jest.fn();
+    const wrapper = shallow(<Menu
+        changeNews={mockClick}
+      /> );
+    wrapper.find('#technology-button').simulate('click');
+    expect(mockClick).toHaveBeenCalled();
+  })
+
+  it('should call the changeNews prop with the entertainment argument when the Entertainment button is clicked', () => {
+    const mockClick = jest.fn();
+    const wrapper = shallow(<Menu
+        changeNews={mockClick}
+      /> );
+    wrapper.find('#entertainment-button').simulate('click');
+    expect(mockClick).toHaveBeenCalled();
+  })
+
+  it('should call the changeNews prop with the science argument when the Science button is clicked', () => {
+    const mockClick = jest.fn();
+    const wrapper = shallow(<Menu
+        changeNews={mockClick}
+      /> );
+    wrapper.find('#science-button').simulate('click');
+    expect(mockClick).toHaveBeenCalled();
+  })
+
+  it('should call the changeNews prop with the health argument when the Health button is clicked', () => {
+    const mockClick = jest.fn();
+    const wrapper = shallow(<Menu
+        changeNews={mockClick}
+      /> );
+    wrapper.find('#health-button').simulate('click');
+    expect(mockClick).toHaveBeenCalled();
+  })
 })
